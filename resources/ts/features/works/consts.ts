19b96ffaced666d6831ab2e68509c870e9f874b9
Kokamasa-19830504 <finom.kokaji.aszu@gmail.com>
2023-03-19 14:26:08 +0900

    update works

--- resources/ts/features/works/consts.ts
+++ resources/ts/features/works/consts.ts
@@ -57,38 +57,38 @@ export const WORKS = [
             },
         ],
     },
-    {
-        label: "Catalog Demo",
-        desc: "ダミーテキストを利用した仮想の商品カタログです。カテゴリ・キーワードで商品を検索できます。",
-        image: "images/works/catalog.webp",
-        fallback_image: "images/works/catalog.jpg",
-        tags: ["Laravel", "GSAP", "SQLite", "Bootstrap"],
-        links: [
-            {
-                label: "Live",
-                href: "https://masapochi.me/catalog/",
-            },
-            {
-                label: "Github",
-                href: "https://github.com/masapochi/products-catalog/",
-            },
-        ],
-    },
-    {
-        label: "Thumbs Game",
-        desc: "みんな知っているけれど、名前はだれも知らないあの親指ゲームです。",
-        image: "images/works/that_game.webp",
-        fallback_image: "images/works/that_game.png",
-        tags: ["Vue.js", "PWA"],
-        links: [
-            {
-                label: "Live",
-                href: "https://masapochi.me/that-game/",
-            },
-            {
-                label: "Github",
-                href: "https://github.com/masapochi/that-game/",
-            },
-        ],
-    },
+    // {
+    //     label: "Catalog Demo",
+    //     desc: "ダミーテキストを利用した仮想の商品カタログです。カテゴリ・キーワードで商品を検索できます。",
+    //     image: "images/works/catalog.webp",
+    //     fallback_image: "images/works/catalog.jpg",
+    //     tags: ["Laravel", "GSAP", "SQLite", "Bootstrap"],
+    //     links: [
+    //         {
+    //             label: "Live",
+    //             href: "https://masapochi.me/catalog/",
+    //         },
+    //         {
+    //             label: "Github",
+    //             href: "https://github.com/masapochi/products-catalog/",
+    //         },
+    //     ],
+    // },
+    // {
+    //     label: "Thumbs Game",
+    //     desc: "みんな知っているけれど、名前はだれも知らないあの親指ゲームです。",
+    //     image: "images/works/that_game.webp",
+    //     fallback_image: "images/works/that_game.png",
+    //     tags: ["Vue.js", "PWA"],
+    //     links: [
+    //         {
+    //             label: "Live",
+    //             href: "https://masapochi.me/that-game/",
+    //         },
+    //         {
+    //             label: "Github",
+    //             href: "https://github.com/masapochi/that-game/",
+    //         },
+    //     ],
+    // },
 ];
